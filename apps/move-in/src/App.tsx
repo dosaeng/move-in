ca0c8a26344b1c5ddce3807625282ceef351eded
kreit-eunjun <eunjun@kreit.co.kr>
2023-12-07 00:16:50 +0900

    feat(move-in): add panda css config

--- apps/move-in/src/App.tsx
+++ apps/move-in/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { IonApp, IonRouterOutlet } from '@move-in/move-in-design-system';
+import { Route, Redirect } from 'react-router-dom';
+import SignUp from './features/sign-up/SignUp';
+import { IonReactRouter } from '@ionic/react-router';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/" render={() => <Redirect to="/sign-up" />} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
 }
 
-export default App
+export default App;
--- packages/move-in-design-system/src/index.ts
+++ packages/move-in-design-system/src/index.ts
@@ -1,4 +1,3 @@
 export { Button } from './buttons/Button';
 export * from './icons/icons';
 export * from '@ionic/react';
-export * from '@ionic/react-router';
