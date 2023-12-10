c4a4371b9f86583e39e0cdefc27ad7a20df5201c
kreit-eunjun <eunjun@kreit.co.kr>
2023-12-10 13:44:47 +0900

    chore(move-in): add path alias setting

--- apps/move-in/vite.config.ts
+++ apps/move-in/vite.config.ts
@@ -1,7 +1,15 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const projectRootDir = path.resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@/", replacement: `${path.join(projectRootDir, 'src')}/` },
+    ]
+  },
 })
