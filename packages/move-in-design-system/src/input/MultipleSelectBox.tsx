07b72f0939416f86841e587046c2b9efaf55eebb
kreit-eunjun <eunjun@kreit.co.kr>
2023-12-12 00:06:48 +0900

    fix(design-system): remove unused variables

--- packages/move-in-design-system/src/input/MultipleSelectBox.tsx
+++ packages/move-in-design-system/src/input/MultipleSelectBox.tsx
@@ -1,50 +1,8 @@
-import { css, cva } from '@move-in/styled-system/css';
+import { css } from '@move-in/styled-system/css';
 import React, { Key } from 'react';
 import { Button } from '../buttons/Button';
 import { Modal } from '../popup/Modal';
 import { SelectBoxModalContent, SelectBoxOption, SelectBoxOptionRow, SelectBoxTrigger } from './SelectBox';
-
-const selectBoxOptionRowStyle = cva({
-  base: {
-    border: '1px solid',
-    borderRadius: '12px',
-    borderColor: 'stroke.light.02',
-    paddingX: '16px',
-    paddingY: '14px',
-    display: 'flex',
-    alignItems: 'center',
-    textStyle: 'body-14-m',
-    color: 'text.dark.03',
-    _hover: {
-      opacity: 0.8,
-    },
-  },
-  variants: {
-    selected: {
-      true: {
-        border: '1.4px solid',
-        borderColor: 'brand.purple.03',
-        color: 'brand.purple.03',
-      },
-    },
-  },
-});
-
-const selectBoxText = cva({
-  base: {
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-    color: 'text.dark.04',
-  },
-  variants: {
-    hasValue: {
-      false: {
-        color: 'text.light.04',
-      },
-    },
-  },
-});
 
 export interface MultipleSelectBoxProps<K, V> {
   value?: SelectBoxOption<K, V>[];
