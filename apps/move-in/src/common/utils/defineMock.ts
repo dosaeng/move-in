d490d19d760b4affacf32425199c174454084486
kreit-eunjun <eunjun@kreit.co.kr>
2023-12-19 23:28:23 +0900

    feat(move-in): add environment value

--- apps/move-in/src/common/utils/defineMock.ts
+++ apps/move-in/src/common/utils/defineMock.ts
@@ -1,7 +1,7 @@
 import type { FetchMockStatic } from "fetch-mock";
 
 export const defineMock = (handler: (mock: FetchMockStatic) => void) => {
-  if (import.meta.env.DEV) {
+  if (import.meta.env.VITE_ENABLE_MOCK_API == 'true') {
     const fetchMock = import("fetch-mock");
 
     fetchMock.then(({ default: mock }) => {
