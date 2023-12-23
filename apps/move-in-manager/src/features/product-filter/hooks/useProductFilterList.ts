1d61712d565dcc777a08c288869aa7727910bf73
kreit-eunjun <eunjun@kreit.co.kr>
2023-12-23 11:42:58 +0900

    feat(move-in-manager): add suggestion status

--- apps/move-in-manager/src/features/product-filter/hooks/useProductFilterList.ts
+++ apps/move-in-manager/src/features/product-filter/hooks/useProductFilterList.ts
@@ -15,25 +15,21 @@ export enum ProductSuggestionState {
 interface ProductFilterListItemDTO {
   id: number;
   name: string;
-  family_type: string;
-  pet_presence: boolean;
-  minimum_size: string;
-  minimum_room_count: number;
-  minimum_move_in_date: string;
-  maximum_move_in_date: string;
+  status: 'DEFAULT_CREATED' | 'OPEN' | 'CLOSE';
   maximum_deposit: number;
-  maximum_monthly_cost: number;
+  preferred_region: string;
+  item_house_type?: string[];
   minimum_monthly_cost: number;
   cost_preference_type: string;
-  preferred_region: string;
-  preferred_village: string;
-  favorite_place1?: string[];
-  item_house_type?: string[];
-  item_house_condition?: string[];
-  item_wish_list?: string[];
-  recommendation_status?: 'REQUESTED' | 'NONE' | 'CONSULTING_REQUESTED',
+  maximum_monthly_cost: number;
   // 제안 만료 날짜
   recommendation_due_date?: string;
+  family_type: string;
+  pet_presence: boolean;
+  preferred_village: string;
+  recommendation_count: number;
+  did_suggest_already: boolean,
+  is_consultation_requested: boolean
 }
 
 export interface ProductFilterListItemModel {
@@ -52,12 +48,23 @@ const useProductFilterList = () => {
     const data = await httpClient.get<ProductFilterListItemDTO[]>(getProductFilterList)
 
     return data.map((item) => {
+      let suggestionState;
+
+      if (item.is_consultation_requested) {
+        suggestionState = ProductSuggestionState.CONSULTING_REQUESTED;
+      } else if (item.did_suggest_already) {
+        suggestionState = ProductSuggestionState.REQUESTED;
+      } else {
+        suggestionState = ProductSuggestionState.NONE;
+      }
 
       return {
         id: item.id,
         name: item.name,
         dueDate: item.recommendation_due_date != null ? new Date(item.recommendation_due_date) : undefined,
         filterList: [
+          item.preferred_region,
+          item.preferred_village,
           item.family_type,
           item.item_house_type,
           `${koreanCurrencyFormat(item.maximum_deposit)} · 월 ${item.maximum_monthly_cost / 10000}-${item.minimum_monthly_cost / 10000}`
@@ -67,7 +74,7 @@ const useProductFilterList = () => {
             value: filterItem,
           };
         }) as { key: number, value: string }[],
-        suggestionState: item.recommendation_status as ProductSuggestionState,
+        suggestionState: suggestionState,
       }
     });
   });
@@ -94,7 +101,9 @@ defineMock((mock) => {
       preferred_region: '서울 / 경기 / 인천',
       preferred_village: '서울특별시 강남구 역삼동',
       item_house_type: ['오피스텔'],
-      recommendation_status: 'NONE',
+      recommendation_count: 1,
+      did_suggest_already: false,
+      is_consultation_requested: false,
     }, {
       id: 2,
       name: '신사 영끌 신혼집 2',
@@ -108,7 +117,10 @@ defineMock((mock) => {
       preferred_region: '서울 / 경기 / 인천',
       preferred_village: '서울특별시 강남구 역삼동',
       item_house_type: ['오피스텔'],
+      recommendation_count: 1,
       recommendation_status: 'CONSULTING_REQUESTED',
+      did_suggest_already: true,
+      is_consultation_requested: true,
     }, {
       id: 3,
       name: '신사 영끌 신혼집 3',
@@ -122,7 +134,9 @@ defineMock((mock) => {
       preferred_region: '서울 / 경기 / 인천',
       preferred_village: '서울특별시 강남구 역삼동',
       item_house_type: ['오피스텔'],
-      recommendation_status: 'REQUESTED',
+      recommendation_count: 1,
+      did_suggest_already: true,
+      is_consultation_requested: false,
     }] as ProductFilterListItemDTO[]), {
       status: 200,
     })
