e335078e08da24cefebe79358fe7b890c37e09a7
kreit-eunjun <eunjun@kreit.co.kr>
2023-12-24 22:47:02 +0900

    chore: add apk build workflow

--- apps/move-in/capacitor.config.ts
+++ apps/move-in/capacitor.config.ts
@@ -1,12 +1,23 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import dotenv from 'dotenv-flow';
+
+dotenv.config();
 
 const config: CapacitorConfig = {
   appId: 'com.kreit.move-in',
   appName: 'MoveIn',
   webDir: 'dist',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
+  android: {
+    buildOptions: {
+      keystoreAlias: process.env.ANDROID_KEYSTORE_KEY_ALIAS,
+      keystoreAliasPassword: process.env.ANDROID_KEYSTORE_KEY_PASSWORD,
+      keystorePassword: process.env.ANDROID_KEYSTORE_PASSWORD,
+      keystorePath: process.env.ANDROID_KEYSTORE_PATH,
+    },
+  },
 };
 
 export default config;
