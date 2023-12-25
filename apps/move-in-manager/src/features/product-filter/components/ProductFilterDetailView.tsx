8fc6dc7cab7c7cb80b5006fbad8b7e37c581402b
kreit-eunjun <eunjun@kreit.co.kr>
2023-12-25 17:49:47 +0900

    fix(move-in-manger): add loading view

--- apps/move-in-manager/src/features/product-filter/components/ProductFilterDetailView.tsx
+++ apps/move-in-manager/src/features/product-filter/components/ProductFilterDetailView.tsx
@@ -1,9 +1,10 @@
+import { IonSpinner } from '@ionic/react';
+import { ChipButtonList, Divider } from '@move-in/design-system';
+import { css, cx } from '@move-in/styled-system/css';
 import { differenceInCalendarDays } from 'date-fns';
 import useProductFilterDetail from '../hooks/useProductFilterDetail';
-import { cx, css } from '@move-in/styled-system/css';
-import ProductSuggestionListView from './ProductSuggestionListView';
 import useProductFilterDetailViewState from '../hooks/useProductFilterDetailViewState';
-import { Divider, ChipButtonList } from '@move-in/design-system';
+import ProductSuggestionListView from './ProductSuggestionListView';
 
 interface Props {
   className?: string;
@@ -12,7 +13,22 @@ interface Props {
 
 const ProductFilterDetailView: React.FC<Props> = ({ className, filterId }) => {
   const { hasSuggestionList } = useProductFilterDetailViewState(filterId);
-  const { data } = useProductFilterDetail(filterId);
+  const { data, isLoading } = useProductFilterDetail(filterId);
+
+  if (isLoading) {
+    return (
+      <div
+        className={css({
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '300px',
+        })}
+      >
+        <IonSpinner color="medium" />
+      </div>
+    );
+  }
 
   return (
     <div
@@ -90,30 +106,54 @@ const ProductFilterDetailView: React.FC<Props> = ({ className, filterId }) => {
         </ProductFilterSubView>
         <ProductFilterSubView title="원하는 집의 조건">
           <ProductFilterSubTagView title="위치">
-            <ChipButtonList options={data?.productPreference.position ?? []} readOnly />
+            <ChipButtonList
+              options={data?.productPreference.position ?? []}
+              readOnly
+            />
           </ProductFilterSubTagView>
           <ProductFilterSubTagView title="매물 구성">
-            <ChipButtonList options={data?.productPreference.type ?? []} readOnly />
+            <ChipButtonList
+              options={data?.productPreference.type ?? []}
+              readOnly
+            />
           </ProductFilterSubTagView>
         </ProductFilterSubView>
         <ProductFilterSubView title="라이프 스타일">
           <ProductFilterSubTagView title="교통권">
-            <ChipButtonList options={data?.lifestylePreference.traffic ?? []} readOnly />
+            <ChipButtonList
+              options={data?.lifestylePreference.traffic ?? []}
+              readOnly
+            />
           </ProductFilterSubTagView>
           <ProductFilterSubTagView title="희망 구성 옵션">
-            <ChipButtonList options={data?.lifestylePreference.livingOption ?? []} readOnly />
+            <ChipButtonList
+              options={data?.lifestylePreference.livingOption ?? []}
+              readOnly
+            />
           </ProductFilterSubTagView>
           <ProductFilterSubTagView title="공동 생활">
-            <ChipButtonList options={data?.lifestylePreference.communityLife ?? []} readOnly />
+            <ChipButtonList
+              options={data?.lifestylePreference.communityLife ?? []}
+              readOnly
+            />
           </ProductFilterSubTagView>
           <ProductFilterSubTagView title="생활권">
-            <ChipButtonList options={data?.lifestylePreference.livingInfra ?? []} readOnly />
+            <ChipButtonList
+              options={data?.lifestylePreference.livingInfra ?? []}
+              readOnly
+            />
           </ProductFilterSubTagView>
           <ProductFilterSubTagView title="학군">
-            <ChipButtonList options={data?.lifestylePreference.educationLife ?? []} readOnly />
+            <ChipButtonList
+              options={data?.lifestylePreference.educationLife ?? []}
+              readOnly
+            />
           </ProductFilterSubTagView>
           <ProductFilterSubTagView title="배달권">
-            <ChipButtonList options={data?.lifestylePreference.deliveryLife} readOnly />
+            <ChipButtonList
+              options={data?.lifestylePreference.deliveryLife}
+              readOnly
+            />
           </ProductFilterSubTagView>
         </ProductFilterSubView>
       </div>
@@ -141,7 +181,9 @@ const DueDate: React.FC<{ date?: Date }> = ({ date }) => {
   return <></>;
 };
 
-const ProductFilterSubView: React.FC<React.PropsWithChildren<{ title: string }>> = ({ title, children }) => {
+const ProductFilterSubView: React.FC<
+  React.PropsWithChildren<{ title: string }>
+> = ({ title, children }) => {
   return (
     <div
       className={css({
@@ -171,10 +213,16 @@ const ProductFilterSubView: React.FC<React.PropsWithChildren<{ title: string }>>
   );
 };
 
-const ProductFilterSubTagView: React.FC<React.PropsWithChildren<{ title: string }>> = ({ title, children }) => {
+const ProductFilterSubTagView: React.FC<
+  React.PropsWithChildren<{ title: string }>
+> = ({ title, children }) => {
   return (
-    <div className={css({ display: 'flex', flexDirection: 'column', gap: '8px' })}>
-      <div className={css({ textStyle: 'body-12-r', color: 'text.dark.01' })}>{title}</div>
+    <div
+      className={css({ display: 'flex', flexDirection: 'column', gap: '8px' })}
+    >
+      <div className={css({ textStyle: 'body-12-r', color: 'text.dark.01' })}>
+        {title}
+      </div>
       {children}
     </div>
   );
