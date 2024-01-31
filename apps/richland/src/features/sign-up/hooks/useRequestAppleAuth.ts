fe29f82d565f8769177db3363625d536ff9a8486
kreit-eunjun <eunjun@kreit.co.kr>
2024-02-01 00:06:37 +0900

    feat(richland): add apple login sdk

--- apps/richland/src/features/sign-up/hooks/useRequestAppleAuth.ts
+++ apps/richland/src/features/sign-up/hooks/useRequestAppleAuth.ts
@@ -1,20 +1,30 @@
 import useSignInState from '@/features/sign-in/hooks/useSignInState';
+import { SignInWithApple } from '@capacitor-community/apple-sign-in';
 import { UseMutationOptions, useMutation } from 'react-query';
+
+export interface AppleLoginTokenData {
+  idToken: string;
+}
 
 // 애플 로그인 시작
 const useRequestAppleAuth = (
   options?: Omit<
-    UseMutationOptions<{ idToken: string }, unknown, void, unknown>,
+    UseMutationOptions<AppleLoginTokenData, unknown, void, unknown>,
     'mutationFn'
   >
 ) => {
   const { setAppleToken } = useSignInState();
 
   return useMutation(async () => {
-    // TODO. 애플 로그인 처리 추가
+    // TODO. 애플 로그인 정보 추가하기
+    const result = await SignInWithApple.authorize({
+      clientId: 'com.kreit.richland',
+      redirectURI: 'localhost:5173',
+      scopes: 'email name',
+    });
 
     const response = {
-      idToken: 'idToken',
+      idToken: result.response.identityToken,
     };
 
     setAppleToken(response);
