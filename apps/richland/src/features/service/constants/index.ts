71148064ace343f82f717a7883826883705cf7ce
kreit-eunjun <eunjun@kreit.co.kr>
2024-03-03 17:55:33 +0900

    feat(richland): add links

---
+++ apps/richland/src/features/service/constants/index.ts
@@ -0,0 +1,4 @@
+export const openSourceLicenseUrl =
+  'https://kreitcokr.wordpress.com/%eb%a6%ac%ec%b9%98%eb%9e%9c%eb%93%9c-%ec%98%a4%ed%94%88%ec%86%8c%ec%8a%a4-%eb%9d%bc%ec%9d%b4%ec%84%bc%ec%8a%a4/';
+
+export const suggestionMailUrl = 'mailto:richland@kreit.co.kr?subject=제안하기';
--- apps/richland/src/features/service/pages/ServicePage.tsx
+++ apps/richland/src/features/service/pages/ServicePage.tsx
@@ -9,6 +9,8 @@ import ServicePageListItem from '../components/ServicePageListItem';
 import ServicePageListLogoutItem from '../components/ServicePageListLogoutItem';
 import ServicePageListNotificationItem from '../components/ServicePageListNotificationItem';
 import ServicePageNavigationBar from '../components/ServicePageNavigationBar';
+import { Browser } from '@capacitor/browser';
+import { openSourceLicenseUrl, suggestionMailUrl } from '../constants';
 
 const horizontalPadding = css({
   paddingX: '20px',
@@ -81,7 +83,9 @@ const ServicePage: React.FC = () => {
           <ServicePageListGroup className={horizontalPadding} title="고객 지원">
             <ServicePageListItem title="고객센터" />
             <ServicePageListItem title="스토어 리뷰하기" />
-            <ServicePageListItem title="제안하기" />
+            <a href={suggestionMailUrl}>
+              <ServicePageListItem title="제안하기" />
+            </a>
           </ServicePageListGroup>
           <Divider size="m" />
           <ServicePageListGroup className={horizontalPadding} title="앱 정보">
@@ -91,7 +95,14 @@ const ServicePage: React.FC = () => {
                 navigate('/service/terms');
               }}
             />
-            <ServicePageListItem title="오픈소스 라이선스" />
+            <ServicePageListItem
+              title="오픈소스 라이선스"
+              onClick={() => {
+                Browser.open({
+                  url: openSourceLicenseUrl,
+                });
+              }}
+            />
             <ServicePageListAppVersionItem />
           </ServicePageListGroup>
           <Divider size="m" />
